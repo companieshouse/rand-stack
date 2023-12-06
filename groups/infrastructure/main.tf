91a6bbb32784ade8b07490d2b7370d744e8f3c7c
joelclemence <jclemence@companieshouse.gov.uk>
2023-12-06 08:20:25 +0000

    Add parameters regarding ALB to parameter store

--- groups/infrastructure/main.tf
+++ groups/infrastructure/main.tf
@@ -73,3 +73,15 @@ resource "aws_ssm_parameter" "rand_domain" {
   type = "SecureString"
   value = local.domain_name
 }
+
+resource "aws_ssm_parameter" "rand_alb_name" {
+  name = "/${local.name_prefix}/alb_name"
+  type = "SecureString"
+  value = module.alb.application_load_balancer_name
+}
+
+resource "aws_ssm_parameter" "rand_alb_sg" {
+  name = "/${local.name_prefix}/alb_security_group_id"
+  type = "SecureString"
+  value = module.alb.security_group_id
+}
