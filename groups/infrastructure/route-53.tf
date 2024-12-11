5330acf501c67319aa41f56826bf2caa9d017f5d
stefano basile <sbasile@companieshouse.gov.uk>
2024-12-11 12:53:35 +0000

    add 'Subject Alternative Name' (pocs.rand.---) to certificate

--- groups/infrastructure/route-53.tf
+++ groups/infrastructure/route-53.tf
@@ -2,6 +2,9 @@ resource "aws_acm_certificate" "certificate" {
   domain_name       = local.domain_name
   validation_method = "DNS"
 
+  # Add additional domains
+  subject_alternative_names = local.alternative_names
+
   tags = {
     Environment = var.environment
   }
@@ -36,6 +39,19 @@ resource "aws_acm_certificate_validation" "certificate" {
 resource "aws_route53_record" "a_record" {
   allow_overwrite = true
   name            = local.domain_name
+  type            = "A"
+  zone_id         = data.aws_route53_zone.hosted_zone.id
+
+  alias {
+    name                   = module.alb.application_load_balancer_dns_name
+    zone_id                = module.alb.application_load_balancer_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "a_record_alternative_1" {
+  allow_overwrite = true
+  name            = local.alternative_name1
   type            = "A"
   zone_id         = data.aws_route53_zone.hosted_zone.id
 
